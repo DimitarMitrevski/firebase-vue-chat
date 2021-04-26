09327704feab476edf777508b71da253a3eca648
natalijakostadinoska <74496725+natalijakostadinoska@users.noreply.github.com>
2021-04-27 00:14:51 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
+import Rooms from "..//views/Rooms.vue"
 
 const routes = [
   {
@@ -20,14 +21,10 @@ const routes = [
     component: Login,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },
+    path: "/rooms",
+    name: "Rooms",
+    component: Rooms,
+  }
 ];
 
 const router = createRouter({
