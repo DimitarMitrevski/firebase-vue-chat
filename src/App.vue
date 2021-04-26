b95da72d47d3f5594d02ad930f4b394c519e1cc4
natalijakostadinoska <74496725+natalijakostadinoska@users.noreply.github.com>
2021-04-27 00:13:41 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <header>
       <ul class="nav">
         <li><router-link to="/login">Login</router-link></li> |
+        <li><router-link to="/rooms">Rooms</router-link></li> |
         <li><router-link to="/register">Register</router-link></li>
         
       </ul>
