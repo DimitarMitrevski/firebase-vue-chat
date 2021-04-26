327d8b1912292a1fb64e25831497d658a25b77a9
natalijakostadinoska <74496725+natalijakostadinoska@users.noreply.github.com>
2021-04-27 00:14:03 +0200

    Update Rooms.vue

--- src/views/Rooms.vue
+++ src/views/Rooms.vue
@@ -1,29 +1,29 @@
 <template>
   
-<div class="grid-container">
-  <div class="item1">Chat 1</div>
-  <div class="item2">Chat 2</div>
-  <div class="item3">Chat 3</div>  
-  <div class="item4">Chat 4</div>
-  <div class="item5">Chat 5</div>
-  <div class="item6">Chat 6</div>
-  <div class="item5">Chat 7</div>
-  <div class="item6">Chat 8</div>
-  
-</div>
-
+  <div class="grid-container">
+    <div class="chat1">
+      <router-link class="item1" to="/register">Chat 1</router-link>
+    </div>
+    <div class="chat2">
+      <router-link class="item2" to="/register">Chat 2</router-link>
+    </div>
+    <div class="chat3">
+    <router-link class="item3" to="/register">Chat 3</router-link>
+    </div>
+    <div class="chat4">
+    <router-link class="item4" to="/register">Chat 4</router-link>
+  </div>
+  </div>
 </template>
 
 <style lang="scss">
-
-
 .grid-container {
   display: grid;
   grid-template-columns: auto auto auto auto;
   grid-template-rows: auto auto auto auto;
   grid-gap: 20px;
   background-color: #bbf157;
-  padding: 170px;
+  padding: 277px;
   margin: auto;
 }
 
@@ -32,7 +32,28 @@
   text-align: center;
   padding: 100px 0;
   font-size: 30px;
+  transition: 0.4s;
+  box-shadow: 0px 6px 12px rgba(0, 0, 0, 0.2);
+  border-radius: 10%;
+  
 }
+
+.grid-container .item1 {
+  text-decoration: none;
+  color: #888;
+}
+.grid-container .item2 {
+  text-decoration: none;
+  color: #888;
+}
+.grid-container .item3 {
+  text-decoration: none;
+  color: #888;
+}
+.grid-container .item4 {
+  text-decoration: none;
+  color: #888;
+}
+
+
 </style>
-
-
