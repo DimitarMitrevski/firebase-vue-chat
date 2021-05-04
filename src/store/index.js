63caf806c1e11245327e1fdb1b32f54c85f2b6a4
Dimitar Mitrevski <71487915+DimitarMitrevski@users.noreply.github.com>
2021-05-04 22:02:14 +0200

    Store initial logic.

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,19 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
+    user: null,
   },
   mutations: {
+    setUser: (state, payload) => {
+      state.user = payload;
+    },
   },
   actions: {
+    //logira korisnikot
+    login: (state, payload) => {
+      //state.commit('setUser',payload);
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
